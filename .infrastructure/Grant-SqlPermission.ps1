e059ed9252faf059b3a163fdb5f8146997e13e9d
Rasmus Lystrøm <rasmusl@microsoft.com>
2021-11-20 01:04:40 +0100

    go

--- .infrastructure/Grant-SqlPermission.ps1
+++ .infrastructure/Grant-SqlPermission.ps1
@@ -18,29 +18,27 @@ begin {}
 process {
     $accessToken = (Get-AzAccessToken -ResourceUrl "https://database.windows.net").Token
 
-    $vars = "Identity='$Identity'"
     $query = @"
         IF NOT EXISTS (
             SELECT * FROM sys.database_principals
-            WHERE [name] = '`$(Identity)' AND [type] = 'E')
+            WHERE [name] = '$Identity' AND [type] = 'E')
         BEGIN
-            CREATE USER [`$(Identity)] FROM EXTERNAL PROVIDER
+            CREATE USER [$Identity] FROM EXTERNAL PROVIDER
         END
 
         IF NOT EXISTS (
             SELECT p.name, o.name FROM sys.database_role_members AS r
             JOIN sys.database_principals AS p ON r.member_principal_id = p.principal_id
             JOIN sys.sysusers AS o ON r.role_principal_id = o.uid
-            WHERE p.name = '`$(Identity)' AND o.name = 'db_owner'
+            WHERE p.name = '$Identity' AND o.name = 'db_owner'
         )
         BEGIN
-            ALTER ROLE db_owner ADD MEMBER [`$(Identity)]
+            ALTER ROLE db_owner ADD MEMBER [$Identity]
         END
 "@
     Invoke-Sqlcmd -ServerInstance "$Server.database.windows.net" `
         -Database $Database `
         -Query $query `
-        -Variable $vars `
         -AccessToken $accessToken
 }
 
