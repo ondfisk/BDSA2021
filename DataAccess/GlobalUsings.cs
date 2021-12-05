3fe02352710a920bfb874ed1b219d273a26a92d2
Rasmus Lystrøm <rasmusl@microsoft.com>
2021-12-05 16:11:05 +0100

    Updated packages (#22)

--- DataAccess/GlobalUsings.cs
+++ DataAccess/GlobalUsings.cs
@@ -1,7 +1,7 @@
+global using Microsoft.Data.SqlClient;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Design;
 global using Microsoft.Extensions.Configuration;
 global using System.ComponentModel.DataAnnotations;
 global using System.Data;
-global using System.Data.SqlClient;
 global using System.IO;
